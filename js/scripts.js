2ab99dbf5ca8a94593805a40d4ba49fc883dd752
Stewart ilondanga <stilondanga@gmail.com>
2021-04-16 14:12:38 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,23 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$(document).ready(function() {
+  $('.table thead tr').mouseover(function() {
+    $('.table tbody tr').addClass('active');
+  }).mouseout(function() {
+    $('.table tbody tr').removeClass('active');
+  });
+  $('.table tbody tr').click(function() {
+    $('#message').css('display', 'block');
+    var message = $(this).find('td').eq(1).html() + '<br><br>' + $(this).data('description');
+    $('#message').animate({
+      height: '0px'
+    }, 600, function() {
+      $('#message').animate({
+        height: '200px'
+      }, 600);
+      $('#message').html(message);
+    });
+  });
+});
